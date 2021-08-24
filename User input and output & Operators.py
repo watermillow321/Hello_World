43350ffe34ebd630d5c9d0cb8f6bf71b8199b129
watermillow321 <89201568+watermillow321@users.noreply.github.com>
2021-08-25 03:43:20 +0800

    User input and output & Operators.py

---
+++ User input and output & Operators.py
@@ -0,0 +1,19 @@
+#user output and input
+
+Jarvs_name = input("Enter a name:") # input from user
+
+print(Jarvs_name) # user print out
+
+#operators
+
+additon = j + a # addition
+substraction = j - a # subtraction
+multiplication = j * a # multiplication
+division = j/a # division
+modulo = j%a # modulo
+
+print(additon)
+print(substraction)
+print(multiplication)
+print(division)
+print(modulo)
---
+++ Variables.py
@@ -0,0 +1,4 @@
+j = 5 # int
+a = 1.15 # float
+jarvis_hello="Hello jarvis " # string
+char = 'p' # char
