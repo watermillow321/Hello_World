7ae4dec0c5e7b3f822e0d4b409a970d60edf87be
watermillow321 <89201568+watermillow321@users.noreply.github.com>
2021-08-25 13:31:18 +0800

    Other Operator

---
+++ Other Operators.py
@@ -0,0 +1,23 @@
+
+# AND Operator
+
+print(y>x and x<=10) # true
+print(x==y and x==10) # true
+
+
+# Both operands should be true so that the outcome could come true, if one operand is false then the whole statement is false.
+
+# OR Operator
+
+print (y>x or x<y) # true
+print (x<=10 or y<=20 ) #true
+print (x==30 or y!=20) #true
+
+# One  operands should be true, if both operand is false then the whole statement is false.
+
+# Not Operator
+
+print(x!=10) # true
+print(x==10) # false
+
+# An operand should be false to become true and false to become true, in short it is just the oposite.
