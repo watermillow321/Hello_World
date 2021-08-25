056f581ee762d2bc31925fdde5b6413f696a6120
watermillow321 <89201568+watermillow321@users.noreply.github.com>
2021-08-25 16:07:58 +0800

    Functions

---
+++ Functions.py
@@ -0,0 +1,23 @@
+# def my_functions(): # This is how you create or declare a function
+  #  print("Hello World")
+
+# my_functions() # This is how you call a funtion
+
+def my_function(name,message): # this are what you call arguments
+    print ("Hello there  " + name + " " + message)
+
+my_function("Will","Welcome to Python")
+
+ # def my_function2(*kids):
+   # print("The youngest child is" + kids[2])
+
+# my_function2("Millow","Milo","Will")
+
+# this will print Will
+
+def my_function1(x):
+    return 5 + x # the return statement returns a value
+
+print(my_function1(6))
+print(my_function1(20))
+print(my_function1(3))
