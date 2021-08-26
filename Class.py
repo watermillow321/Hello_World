1ac35e63ec002d520316ba2dc747a5cf0f27c01e
watermillow321 <89201568+watermillow321@users.noreply.github.com>
2021-08-27 00:59:12 +0800

    Class with Inheritance

--- Class.py
+++ Class.py
@@ -17,34 +17,54 @@
 # p1.age = "30"
 # p1.output()
 
-class A:
-    def function1(self):
-        print("My first function")
+# class A:
+  #  def function1(self):
+     #   print("My first function")
 
-    def function2(self):
-        print("My second function")
+  #  def function2(self):
+     #   print("My second function")
 
-class B:
-    def function3(self):
-        print("My third function")
+# class B:
 
-    def function4(self):
-         print("My fourth function")
-class C(A,B):
-    def function5(self):
-        print("My fifth function")
+   # def function3(self):
+     #   print("My third function")
 
-R1 = A()
+   # def function4(self):
+       #  print("My fourth function")
+# class C(A,B):
+    # def function5(self):
+       # print("My fifth function")
 
-R1.function1()
-R1.function2()
+# R1 = A()
 
-R2 = B()
+# R1.function1()
+# R1.function2()
 
-R2.function1()
-R2.function2()
+# R2 = B()
 
-R3 = C()
+# R2.function1()
+# R2.function2()
 
-R3.function4()
-R3.function5()
+# R3 = C()
+
+# R3.function4()
+# R3.function5()
+
+
+class init_class:
+    def __init__(self, first_name, last_name, message):
+        self.firstname = first_name
+        self.lastname = last_name
+        self.message = message
+
+
+class Student(init_class):
+    def __init__(self,firstname,lastname,message,Age):
+        super().__init__(firstname,lastname,message)
+        self.persons_Age = Age
+
+    def welcome_message(self):
+        print("Welcome" + " " + self.firstname + "'" + self.lastname + " " + self.message + " your " + self.persons_Age + " year's old")
+
+R1 = Student("Will", "Carpio" , " Weclome to Python","19" )
+R1.welcome_message()
