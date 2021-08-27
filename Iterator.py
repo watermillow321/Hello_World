40e9f40dabb9c8cb2c6ca7a55fc2df90450cf75a
watermillow321 <89201568+watermillow321@users.noreply.github.com>
2021-08-28 02:41:25 +0800

    still working on it

---
+++ Iterator.py
@@ -0,0 +1,16 @@
+# numbers = [8,10,15,19,20]
+
+# it = iter(numbers) # we declare the iterator
+
+ # print(it.__next__()) this will print 8
+ # print(it.__next__()) this will print 10
+
+# print(next(it)) this is also another way to print an iterator
+
+class numbers:
+    def __init__(self):
+        self.num = 1
+
+
+
+
