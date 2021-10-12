d22cbd7530e67f72384f93d8b1a6acdd30945915
watermillow321 <89201568+watermillow321@users.noreply.github.com>
2021-10-13 02:29:36 +0800

    still working on it

--- List.py
+++ List.py
@@ -59,10 +59,53 @@ for x in names:
 
 print(newlist)
 
-newlist = [x for x in names if "o" in x]
+newlist = [x for x in names if "o" in x] # prints name with a 'o'
 
 print(newlist)
 
 lewlist = [x for x in names if x != "Millow"] # removes an element in a list but leaves the original list unchanged
 
 print(lewlist)
+
+mewlist = [x for x in range(10)] # prints numbers to 0 to 9
+
+print(mewlist)
+
+rewlist = [x for x in range(10) if x < 5 ] # will print numbers 0 to 4
+
+print(rewlist)
+
+ewlist = ['hello' for x in names] # replaces all the elements in the list with 'hello'
+print(ewlist)
+
+orlist = [ x if x != 'Millow' else 'Milo' for x in names] # changes the 'Millow' element from the list and changes it to 'Milo'
+
+print(orlist)
+
+# sort method
+
+names.sort() # sorts name alphabetically
+print(names)
+
+TheNumbers = [20,19,209,123,145,56543,6373,74567,345]
+TheNumbers.sort() # can also be used to sort numbers from smallest to highest
+
+names.sort(reverse = True) # to sort list in a descending order
+print(names)
+
+TheNumbers.sort(reverse=True)  # to sort list in a descending order
+print(TheNumbers)
+
+def sortings(x): # this function is used for customization of a sorting method
+    return abs(x - 1000)
+
+NewNumber = [ 500,200,300,50,100]
+NewNumber.sort(key = sortings) # this is what is needed to declear the customized sorting method
+print(NewNumber)
+
+fruit = [ 'Orange','Banana','apple','coconut']
+fruit.sort(key = str.upper) # this sorts out the lower case elements first and the upper case elements inside the list
+print(fruit)
+
+fruit.reverse() # prints out the elements in reverse regradless of the arrangements
+print(fruit)
